69bf9ee22eb603072ed8a323d74f8e1950de93dd
Francesco Cappetti <f.cappetti.05@gmail.com>
2022-07-09 16:08:28 +0200

    AST printer

--- plox/ast_printer.py
+++ plox/ast_printer.py
@@ -1,9 +1,23 @@
-from expr import T, Expr, ExprVisitor, Binary, Grouping, Literal, Unary
-from typing import Generic
+from expr import Expr, ExprVisitor, Binary, Grouping, Literal, Unary
+from tokens import Token, TokenType
+from typing import Any
 
 
-class AstPrinter(ExprVisitor, Generic[T]):
-    def print(self, expr: Expr) -> T:
+class AstPrinter(ExprVisitor):
+    @staticmethod
+    def main() -> None:
+        expression = Binary(
+            Unary(
+                Token(TokenType.MINUS, "-", None, 1),
+                Literal(123),
+            ),
+            Token(TokenType.STAR, "*", None, 1),
+            Grouping(Literal(45.67)),
+        )
+
+        print(AstPrinter().print(expression))
+
+    def print(self, expr: Expr) -> Any:
         return expr.accept(self)
 
     def parenthesize(self, name: str, *exprs: Expr) -> str:
@@ -15,16 +29,20 @@ class AstPrinter(ExprVisitor, Generic[T]):
 
         return builder
 
-    def visit_binary_expr(self, expr: Binary) -> T:
+    def visit_binary_expr(self, expr: Binary) -> str:
         return self.parenthesize(expr.operator.lexeme, expr.left, expr.right)
 
-    def visit_grouping_expr(self, expr: Grouping) -> T:
+    def visit_grouping_expr(self, expr: Grouping) -> str:
         return self.parenthesize("group", expr.expression)
 
-    def visit_literal_expr(self, expr: Literal) -> T:
+    def visit_literal_expr(self, expr: Literal) -> str:
         if expr.value is None:
             return "nil"
         return str(expr.value)
 
-    def visit_unary_expr(self, expr: Unary) -> T:
+    def visit_unary_expr(self, expr: Unary) -> str:
         return self.parenthesize(expr.operator.lexeme, expr.right)
+
+
+if __name__ == "__main__":
+    AstPrinter.main()
